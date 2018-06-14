3217d316b6deef7fa5c95f166ce2fb3de7f9ed9d
Guest User <Guest@epicodus-57.local>
2018-06-14 10:33:54 -0700

    added an array and an object containing the array

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+/***************************** EXAMPLES ******************************/
+//OBJECT EXAMPLE
+// var epicodusStudent = {
+//   firstName: "Charlie",
+//   lastName: "Bucket",
+//   level: 1,
+//   track: ["PHP","JavaScript","Drupal"],
+//   enrollmentStatus: true
+// };
+
+
+// CONSTRUCTOR EXAMPLE
+// var dog1 = new Dog("Falcor", ["black"], 4);
+
+
+///////////////// Constructors to be used in chess game /********************/
+// Player, Space, Board, and Game
+
+
+
+
+
+/***************************** BACK END ******************************/
+//OBJECT
+
+//THIS IS AN ARRAY
+var boardArray = [
+  ['','',''],
+  ['','',''],
+  ['','','']
+];
+
+//THIS IS AN OBJECT
+var gameBoard = {
+  board: boardArray
+};
+
+/*******************  FRONT END (USER INTERFACE) *********************/
